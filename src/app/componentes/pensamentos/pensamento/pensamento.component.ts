6192ce4a961fd76bf9710361746f1d6ca9f31ff1
MatheusCorreaaa <mcorreia073@gmail.com>
2024-07-09 03:34:45 -0300

    primeiro commit

---
+++ src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -0,0 +1,45 @@
+import { PensamentoService } from '../pensamento.service';
+import { Pensamento } from './../pensamento';
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pensamento',
+  templateUrl: './pensamento.component.html',
+  styleUrls: ['./pensamento.component.css']
+})
+export class PensamentoComponent implements OnInit {
+
+  @Input() pensamento: Pensamento = {
+    id: 0,
+    conteudo: 'I love Angular',
+    autoria: 'Nay',
+    modelo: 'modelo3',
+    favorito: false
+  }
+
+  @Input() listaFavoritos: Pensamento[] = [];
+
+  constructor(private service:PensamentoService) { }
+
+  ngOnInit(): void {
+  }
+
+  larguraPensamento(): string {
+    if(this.pensamento.conteudo.length >= 256) {
+      return 'pensamento-g'
+    }
+    return 'pensamento-p'
+  }
+
+  mudarIconeFavorito():string {
+    if(this.pensamento.favorito == false){
+      return 'inativo'
+    }
+    return 'ativo'
+  }
+  atualizarFavoritos(){
+    this.service.mudarfavorito(this.pensamento).subscribe(()=>{
+      this.listaFavoritos.splice(this.listaFavoritos.indexOf(this.pensamento),1)
+    });
+  }
+}
